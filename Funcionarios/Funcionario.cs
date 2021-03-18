e3458fe31d3daecb9aab77cf8b8dcc8ce89486e4
Bruno Henrique <bruno.henrique@inovafarma.com.br>
2021-03-17 23:14:16 -0300

    Aula 1 - Atividaede 2 Atalhos no VisualStudio.

---
+++ Funcionarios/Funcionario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        //0 - funcionario
+        //1 - diretor
+        //2 - designer
+        //N - ....
+        
+        private int _tipo;
+
+        public string Nome{ get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(int tipo)
+        {
+            this._tipo = tipo;
+        }
+
+        public double GetBonificacao()
+        {
+
+            if (this._tipo == 1) 
+            {
+                return Salario;
+            }
+            return Salario * 0.10;
+        }
+    }
+}
